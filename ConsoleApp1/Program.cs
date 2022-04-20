3badbd1ef374df3df47beec12257f88481eb6b0d
Vitalii Loktyev <34657517+shon327@users.noreply.github.com>
2022-04-20 15:50:03 +0300

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,16 +1,10 @@
 using System;
-
 namespace Test
 {
-
-
     public class Program
     {
-
-
         public static void Main(string[] args)
         {
-
             System.Console.WriteLine("Hello World");
         }
     }
