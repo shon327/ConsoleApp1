32c19b9a5168cdf33f0fca68676c01f96e9dc40f
Vitalii Loktyev <34657517+shon327@users.noreply.github.com>
2022-04-20 15:47:54 +0300

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace Test
+{
+
+
+    public class Program
+    {
+
+
+        public static void Main(string[] args)
+        {
+
+            System.Console.WriteLine("Hello World");
+        }
+    }
+}
